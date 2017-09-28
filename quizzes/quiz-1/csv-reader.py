fc4960e26e7103c52d7776366598773bc88e0603
minaamfasihi <minaam.fasihi@gmail.com>
2017-09-28 01:17:52 -0700

    Updated path for resources

--- quizzes/quiz-1/csv-reader.py
+++ quizzes/quiz-1/csv-reader.py
@@ -1,6 +1,6 @@
 import unicodecsv
 
-with open('Resources\enrollments.csv', 'rb') as f:
+with open('..\..\..\Resources\enrollments.csv', 'rb') as f:
 	reader = unicodecsv.DictReader(f)
 	enrollments = list(reader)
 
