aeda5c6ce1e07c4af0e72efcb9b678a2014ad7c8
minaamfasihi <minaam.fasihi@gmail.com>
2017-09-28 01:18:34 -0700

    Added code to read daily_engagement

--- quizzes/quiz-1/csv-reader.py
+++ quizzes/quiz-1/csv-reader.py
@@ -5,3 +5,9 @@ with open('..\..\..\Resources\enrollments.csv', 'rb') as f:
 	enrollments = list(reader)
 
 print enrollments[0]
+
+with open('..\..\..\Resources\daily_engagement.csv', 'rb') as f:
+	reader = unicodecsv.DictReader(f)
+	daily_engagement = list(reader)
+
+print daily_engagement[0]
