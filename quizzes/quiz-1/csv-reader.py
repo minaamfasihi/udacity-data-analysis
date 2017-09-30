5ec522bc3a02110607a901725a3ad198d7d8ebb9
minaamfasihi <minaam.fasihi@gmail.com>
2017-09-30 04:27:50 -0700

    Wrote helper methods to fix data types

--- quizzes/quiz-1/csv-reader.py
+++ quizzes/quiz-1/csv-reader.py
@@ -1,4 +1,5 @@
 import unicodecsv
+from datetime import datetime as dt
 
 def read_file(path, mode):
 	with open(path, mode) as f:
@@ -6,6 +7,18 @@ def read_file(path, mode):
 		csv_file = list(handler)
 
 	return csv_file
+
+def parse_date(date):
+  if date == '':
+    return None;
+  else:
+    return dt.strptime(date, '%Y-%m-%d')
+
+def parse_maybe_int(i):
+  if i == '':
+    return None;
+  else:
+    return int(i)
 
 enrollments = read_file('..\..\..\Resources\enrollments.csv', 'rb')
 
