a3a83aa20f4adb1f1571ca6c66e6e59547af621c
minaamfasihi <minaam.fasihi@gmail.com>
2017-09-30 07:19:18 -0700

    Refactored code to get the unique students and renamed the acct in engagements table

--- quizzes/quiz1/csvreader.py
+++ quizzes/quiz1/csvreader.py
@@ -20,8 +20,15 @@ def parse_maybe_int(i):
   else:
     return int(i)
 
+def get_unique_students(data):
+  unique_students = set()
+  for data_point in data:
+    unique_students.add(data_point['account_key'])
+
+  return unique_students
+
 enrollments = read_file('..\..\..\Resources\enrollments.csv', 'rb')
-enrollments_set = set()
+unique_enrollments_student = get_unique_students(enrollments)
 
 for enrollment in enrollments:
   enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
@@ -29,13 +36,11 @@ for enrollment in enrollments:
   enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
   enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
   enrollment['join_date'] = parse_date(enrollment['join_date'])
-  enrollments_set.add(enrollment['account_key'])
 
-print "Total number of rows: ", enrollments.__len__()
-print "Total number of unique students: ", enrollments_set.__len__()
+print "Total number of rows: ", len(enrollments)
+print "Total number of unique students: ", len(unique_enrollments_student)
 
 daily_engagement = read_file('..\..\..\Resources\daily_engagement.csv', 'rb')
-daily_engagement_set = set()
 
 for engagement_record in daily_engagement:
   engagement_record['lessons_completed'] = int(float(engagement_record['lessons_completed']))
@@ -43,18 +48,20 @@ for engagement_record in daily_engagement:
   engagement_record['projects_completed'] = int(float(engagement_record['projects_completed']))
   engagement_record['total_minutes_visited'] = float(engagement_record['total_minutes_visited'])
   engagement_record['utc_date'] = parse_date(engagement_record['utc_date'])
-  daily_engagement_set.add(engagement_record['acct'])
+  engagement_record['account_key'] = engagement_record['acct']
+  del engagement_record['acct']
 
-print "Total number of rows ",  daily_engagement.__len__()
-print "Total number of unique students: ", daily_engagement_set.__len__()
+unique_daily_engagement_students = get_unique_students(daily_engagement)
+
+print "Total number of rows ",  len(daily_engagement)
+print "Total number of unique students: ", len(unique_daily_engagement_students)
 
 project_submissions = read_file('..\..\..\Resources\project_submissions.csv', 'rb')
-project_submissions_set = set()
+unique_project_submitters = get_unique_students(project_submissions)
 
 for submission in project_submissions:
   submission['completion_date'] = parse_date(submission['completion_date'])
   submission['creation_date'] = parse_date(submission['creation_date'])
-  project_submissions_set.add(submission['account_key'])
 
-print "Total number of rows", project_submissions.__len__()
-print "Total number of unique students", project_submissions_set.__len__()
+print "Total number of rows", len(project_submissions)
+print "Total number of unique students", len(unique_project_submitters)
